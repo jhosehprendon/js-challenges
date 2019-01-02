00d330e49110106973123d0166b97f0e8ed7c3db
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2019-01-01 19:06:24 -0500

    Grading students algoritthm

--- hackerrank.js
+++ hackerrank.js
@@ -1,3 +1,50 @@
+////////////////////
+////////////////////
+////////////////////
+///// ARRAYS ///////
+////////////////////
+////////////////////
+////////////////////
+
+
+// Complete the hourglassSum function below.
+function hourglassSum(arr) {
+    let maxI = 3;
+    let maxJ = 3;
+    let result = -64;
+
+    for (let i = 0; i <= maxI; i++) {
+        for (let j = 0; j <= maxJ; j++) {
+            // sum for the top of hourglass
+            let sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+
+            // add the middle of hourglass
+            sum += arr[i + 1][j + 1];
+
+            // add the bottom of hourglass
+            sum += arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]
+
+            if (result < sum)
+                result = sum;
+        }
+    }
+    return result;
+}
+
+// Complete the rotLeft function below.
+function rotLeft(a, d) {
+    var arr = [];
+    for (var i = 0; i < a.length; i++) {
+        arr.push(a[i]);
+    };
+    for (var j = 1; j <= d; j++) {
+        arr.shift(arr.push(arr[0]));
+    }
+    return arr;
+
+}
+
+
 // New Year Chaos Hackerrank - Minimum number of bribes
 
 function minimumBribes(arr) {
@@ -25,3 +72,23 @@ console.log(minimumBribes([2, 5, 1, 3, 4])) // 'Too Chaotic'
 
 console.log(minimumBribes([5, 1, 2, 3, 7, 8, 6, 4])) // 'Too Chaotic'
 console.log(minimumBribes([1, 2, 5, 3, 7, 8, 6, 4])) // 7
+
+
+////////////////////
+////////////////////
+////////////////////
+//// ALGORITHMS ////
+////////////////////
+////////////////////
+////////////////////
+
+
+// Complete the gradingStudents function below.
+ 
+function gradingStudents(grades) {
+    return grades.map(grade =>  {
+        return (grade >= 38 && grade % 5 >= 3) ? grade + 5 - (grade % 5) : grade;
+    });
+}
+
+console.log(gradingStudents([73, 67, 38, 33, 39])) 
